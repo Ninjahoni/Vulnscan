b2ec010b6e88e9dd40dae58e15540237853b5fa8
Ninjahoni <nigeshshrestha8@gmail.com>
2026-02-06 12:52:37 +0545

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,33 +1,30 @@
 import tkinter as tk
 from tkinter import messagebox, scrolledtext
-from scanner import Scanner
+from Scanner import Scanner  
+import ipaddress
 
 class VulnScanGUI:
     def __init__(self, root):
         self.root = root
-        self.root.title("VulnScanPro - Educational Vulnerability Scanner")
+        self.root.title("VulnScan - By Nigesh Shrestha")
         self.root.geometry("600x450")
         self.root.resizable(False, False)
-
         self.scanner = Scanner()
-
-        # Title
+      
         tk.Label(
             root,
-            text="VulnScanPro",
+            text="VulnScan",
             font=("Arial", 18, "bold")
         ).pack(pady=10)
 
-        # Target Input
         frame = tk.Frame(root)
         frame.pack(pady=5)
-
+        
         tk.Label(frame, text="Target IP:").grid(row=0, column=0, padx=5)
         self.target_entry = tk.Entry(frame, width=30)
         self.target_entry.grid(row=0, column=1)
         self.target_entry.insert(0, "127.0.0.1")
 
-        # Scan Button
         tk.Button(
             root,
             text="Start Scan",
@@ -36,28 +33,37 @@ class VulnScanGUI:
             fg="white",
             width=20
         ).pack(pady=10)
-
-        # Output Box
+      
         self.output = scrolledtext.ScrolledText(
             root,
             width=70,
             height=15
         )
         self.output.pack(padx=10, pady=10)
-
-        # Footer
+        
         tk.Label(
             root,
-            text="For educational and ethical use only",
+            text="CW1 Project",
             font=("Arial", 9, "italic")
         ).pack(pady=5)
-
+   
+    def is_valid_ip(self, ip):
+        try:
+            ipaddress.ip_address(ip)
+            return True
+        except ValueError:
+            return False
+ 
     def start_scan(self):
         target = self.target_entry.get().strip()
         ports = [21, 22, 80, 443]
 
         if not target:
             messagebox.showerror("Error", "Please enter a target IP")
+            return
+
+        if not self.is_valid_ip(target):
+            messagebox.showerror("Error", "Invalid IP Address")
             return
 
         self.output.delete("1.0", tk.END)
